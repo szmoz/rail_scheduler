650057759c233e9af3e7119eb982fb6147651231
Mozes Szanto <szmoz1992@gmail.com>
2024-03-05 21:48:06 +0100

    Create basic color set

---
+++ code/util/colors.py
@@ -0,0 +1,20 @@
+class Colors:
+    """
+    Default colors
+    """
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    LIGHT_GRAY = (200, 200, 200)
+    LIGHTER_GRAY = (225, 225, 225)
+    DARK_GRAY = (140, 140, 140)
+    DARKER_GRAY = (80, 80, 80)
+    BLUE = (0, 0, 255)
+    LIGHT_BLUE = (0, 50, 255)
+    PINK = (255, 215, 215)
+    RED = (255, 0, 0)
+    DARK_RED = (25, 0, 0)
+    GREEN = (0, 128, 0)
+    PURPLE = (128, 0, 128)
+    BROWN = (128, 64, 0)
+    CYAN = (255, 0, 255)
+    
