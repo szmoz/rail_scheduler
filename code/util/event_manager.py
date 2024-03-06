613641d1091bbbe6dc1142a32006f6008e1c0700
Mozes Szanto <szmoz1992@gmail.com>
2024-03-06 13:25:47 +0100

    Create event_manager object

---
+++ code/util/event_manager.py
@@ -0,0 +1,40 @@
+import pygame as pg
+
+
+class EventManager:
+    """
+    Event manager for pygame events
+    """
+    def __init__(self,
+                 event_types: tuple or list,
+                 event_functions: tuple or list,
+                 ) -> None:
+        """
+        Initialize event manager
+        :param event_types: list of pygame event type codes
+        :param event_functions: list of event handler functions in identical order
+        """
+        if len(event_functions) != len(event_types):
+            raise ValueError("Not identical amount of elements in event_types and event_functions")
+        
+        self.event_handlers = dict()
+        for i in range(len(event_types)):
+            self.event_handlers[event_types[i]] = event_functions[i]
+        print(self.event_handlers)
+
+    def handle(self,
+               event: pg.event.Event = None,
+               game=None,
+               ):
+        """
+        Handle event
+        :param event: pygame event
+        :param game: Game object
+        :return: bool True:action happened, go to next event; False:action did not happen, go to next event manager
+        """
+        print(event, game)
+        try:
+            return self.event_handlers[event.type](event, game)
+        except KeyError:
+            return False
+        
