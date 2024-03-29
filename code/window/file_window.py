ddae67e9e0e4ea101aa1179b9268a0d5b9cf3db3
Mozes Szanto <szmoz1992@gmail.com>
2024-03-29 21:50:47 +0100

    Add FileNameList and update frame_edge_line parameters

--- code/window/file_window.py
+++ code/window/file_window.py
@@ -1,14 +1,16 @@
 import pygame as pg
 
 from code.util.button import Button
-from code.util.textbox import TextBox
+from code.util.textbox import FileWinTextBox
 
 from code.window.color_data import FileColors as Colors
 from code.window.color_data import Colors as WinColors
+from code.window.file_name_list import FileNameList
 from code.window.size_data import FileWinSizes as Sizes
 from code.window.size_data import Sizes as WinSizes
 from code.window.string_data import FileStrings as Strings
 from code.window.string_data import Strings as WinStrings
+from code.window.variable_data import FileWinFrameEdges as FrameEdges
 from code.window.window import Window
 
 
@@ -56,7 +58,6 @@ class FileWindow(Window):
             title_size=WinSizes.TITLE,
             title_type=WinStrings.TITLE_TYPE,
         )
-        # List ------------------------------------------------------------
         # Buttons
         button_left = self.rect.right - self.frame.thickness - int(Sizes.BUTTON_WIDTH * 1.5)
         button_top = self.rect.bottom - self.frame.thickness - (Sizes.BUTTON_HEIGHT * 2)
@@ -81,7 +82,7 @@ class FileWindow(Window):
                 frame_top_color_pressed=WinColors.BUTTON_FRAME_TOP_PRESSED,
                 frame_bottom_color_pressed=WinColors.BUTTON_FRAME_BOTTOM_PRESSED,
                 background_color_pressed=WinColors.BUTTON_BACKGROUND_PRESSED,
-                frame_edge_lines_pressed=(3, 4),
+                frame_edge_lines_pressed=FrameEdges.BUTTON_PRESSED,
             )
             button_top -= int(Sizes.BUTTON_HEIGHT * 1.5)
         # Corner button
@@ -108,27 +109,43 @@ class FileWindow(Window):
             image_path='resources/graphics/corner_button.png',
         )
         # Textbox
-        self.textbox = TextBox(
+        self.textbox = FileWinTextBox(
             rect=pg.Rect(
                 self.topline.rect.left + int(Sizes.BUTTON_WIDTH * 0.25),
                 button_top + int(Sizes.BUTTON_HEIGHT * 1.5),
-                button_left - self.topline.rect.left - int(Sizes.BUTTON_WIDTH * 0.5),
+                button_left - self.topline.rect.left - int(Sizes.BUTTON_WIDTH * 4.2),
                 Sizes.TEXTBOX_HEIGHT
             ),
             frame_thickness=Sizes.TEXTBOX_FRAME_THICKNESS,
             frame_top_color=Colors.TEXTBOX_FRAME_TOP,
             frame_bottom_color=Colors.TEXTBOX_FRAME_BOTTOM,
-            frame_edge_lines=(-3, -4),
+            frame_edge_lines=FrameEdges.TEXTBOX,
             text_size=Sizes.TEXTBOX_TEXT,
             text_type=Strings.TEXTBOX_TYPE,
             align="left",
             start_text=start_text,
         )
+        # List
+        self.list = FileNameList(
+            rect=pg.Rect(
+                self.topline.rect.left + int(Sizes.BUTTON_WIDTH * 0.25),
+                self.topline.rect.bottom + self.topline.rect.height,
+                self.topline.rect.width - int(Sizes.BUTTON_WIDTH * 0.5),
+                (self.textbox.rect.top - Sizes.BUTTON_HEIGHT) - (self.topline.rect.bottom + self.topline.rect.height),
+            ),
+            frame_thickness=Sizes.LIST_FRAME_THICKNESS,
+            frame_top_color=Colors.LIST_FRAME_TOP,
+            frame_bottom_color=Colors.LIST_FRAME_BOTTOM,
+            frame_edge_lines=FrameEdges.LIST,
+            text_size=Sizes.LIST_TEXT,
+            text_type=Strings.LIST_TYPE,
+            file_type=self.file_type,
+        )
         
         # Event management
         self.event_managers = (
             self.textbox.event_manager,
-            self.button_event_manager,  # need to add list and textbox event managers
+            self.list.event_manager,
         )
         
     def event_manager(self,
@@ -142,12 +159,17 @@ class FileWindow(Window):
         :return: True: go to next event; False: go to next event manager
         """
         for event_manager in self.event_managers:
-            if program.break_event_loop:
-                return True
             if event_manager(
                     event=event,
-                    program=program,):
+                    program=program,
+                    file_win=self):
                 return True
+            if program.break_event_loop:
+                return True
+        if self.button_event_manager(
+                event=event,
+                program=program):
+            return True
         return False
     
     def is_file_name_exist(self):
@@ -160,9 +182,15 @@ class FileWindow(Window):
     def draw(self,
              surf: pg.Surface,
              ) -> pg.Rect:
+        """
+        Draw File Window on surface
+        :param surf: surface
+        :return: rectangle area for program.draw_rect
+        """
         # Basic content
         self.draw_basic(surf)
-        # List ------------------------------------------------------------
+        # List
+        self.list.draw(surf)
         # Textbox
         self.textbox.draw(surf)
         return self.rect
@@ -176,7 +204,8 @@ class FileWindow(Window):
         """
         # Basic content
         x_diff, y_diff = self.reposition_basic(new_screen_center)
-        # List ------------------------------------------------------------
+        # List
+        self.list.reposition(x_diff, y_diff)
         # Textbox
         self.textbox.reposition(x_diff, y_diff)
         
