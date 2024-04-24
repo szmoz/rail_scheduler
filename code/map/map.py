13363155ce09a2f2b4e4be4e5a2f3e92f549059f
Mozes Szanto <szmoz1992@gmail.com>
2024-04-24 16:41:25 +0200

    Update comments and rename variables

--- code/map/map.py
+++ code/map/map.py
@@ -4,47 +4,80 @@ from code.program.states import FileStates
 
 
 class Map:
+    """
+    Map object. Contains all map data and schedule data
+    """
     def __init__(self):
+        """
+        Initialize an empty Map object
+        """
         # Data
         self.data = {}
+        
         # File variables
         self.file_name = None
-        self.state = FileStates.NO
+        self.file_state = FileStates.NO
+        
+    # Data management functions
     
+    
+    # File handling functions
     def load(self,
              program,
              file_name: str,
              ) -> None:
+        """
+        Load map
+        :param program: Program object
+        :param file_name: name of map file to load
+        """
         self.set_file_name(
             program=program,
             new_file_name=file_name,
         )
-        self.state = FileStates.SAVED
+        self.file_state = FileStates.SAVED
     
     def save(self,
              program,
              file_name: str = None,
              ) -> None:
+        """
+        Save map
+        :param program: Program object
+        :param file_name: name of map file to save
+        """
         if file_name is not None:
             self.set_file_name(
                 program=program,
                 new_file_name=file_name,
             )
-        self.state = FileStates.SAVED
+        self.file_state = FileStates.SAVED
         
-    def delete_progress(self):
-        self.state = FileStates.SAVED
+    def delete_progress(self) -> None:
+        """
+        Delete unsaved progress
+        """
+        self.file_state = FileStates.SAVED
         
-    def create_empty(self, program):
+    def create_empty(self, program) -> None:
+        """
+        Create empty map
+        :param program: Program object
+        """
         self.set_file_name(
             program=program,
         )
-        self.state = FileStates.EMPTY
+        self.file_state = FileStates.EMPTY
         
     def set_file_name(self,
                       program,
                       new_file_name: str = None,
                       ) -> None:
+        """
+        Set map file name and window caption
+        :param program: Program object
+        :param new_file_name: new map file name
+        """
         self.file_name = new_file_name
         program.caption[1] = self.file_name
         program.caption[2] = None
