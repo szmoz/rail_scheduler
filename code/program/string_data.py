d6372a9f2b5b3d65cbee2d0194855c9a6359b295
Mozes Szanto <szmoz1992@gmail.com>
2024-04-25 15:38:24 +0200

    Add file passwords

--- code/program/string_data.py
+++ code/program/string_data.py
@@ -10,4 +10,10 @@ class Strings:
         FileTypes.GAME: "game",
         FileTypes.SAVED_GAME: "saved game"
     }
+    FILE_PASSWORDS = {
+        FileTypes.MAP: bytes("This is a rail scheduler map   file", 'utf-8'),
+        FileTypes.SIM: bytes("This is a rail scheduler sim   file", 'utf-8'),
+        FileTypes.GAME: bytes("This is a rail scheduler game  file", 'utf-8'),
+        FileTypes.SAVED_GAME: bytes("This is a rail scheduler sgame file", 'utf-8'),
+    }
     
