267f2a836c8c16edb591e722696bf25611ae9f83
Mozes Szanto <szmoz1992@gmail.com>
2024-05-12 21:35:50 +0200

    Add platform image creator

--- code/util/create_rail_images.py
+++ code/util/create_rail_images.py
@@ -21,6 +21,22 @@ def create_img(rail_type: str):
         f"C:/Users/M/PycharmProjects/rail_scheduler_v2/resources/graphics/rail_{rail_type}.BMP"
     )
     
+def create_platform():
+    img = pg.image.load(
+        f"C:/Users/M/PycharmProjects/rail_scheduler_v2/resources/graphics/creation/platform.bmp").convert()
+    size = img.get_size()
+    new_img = pg.Surface(size)
+    new_img.fill("black")
+    new_img.set_colorkey("black")
+    for y in range(size[1]):
+        for x in range(size[0]):
+            px = img.get_at((x, y))
+            if px[:3] == (0, 0, 0):
+                new_img.set_at((x, y), (255, 128, 0))
+    pg.image.save(
+        new_img,
+        f"C:/Users/M/PycharmProjects/rail_scheduler_v2/resources/graphics/platform.BMP"
+    )
     
 rail_types = ("straight", "diag", "turn", "diagturn", "bend", "diagbend", "diag_corner")
 for i in range(len(rail_types)):
